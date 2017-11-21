1cae62d16f956ec10457673c48dceb72441ea972
alexbgameson <alexbgames1@gmail.com>
2017-11-21 08:29:05 -0500

    Add files via upload

---
+++ Buycraft/src/Buycraft/PocketMine/Util/RunAsyncTask.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Buycraft\PocketMine\Util;
+
+
+use Buycraft\PocketMine\BuycraftPlugin;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\Server;
+
+class RunAsyncTask extends PluginTask{
+	private $asyncTask;
+
+	/**
+	 * RunAsyncTask constructor.
+	 * @param $plugin
+	 * @param $asyncTask
+	 */
+	public function __construct(BuycraftPlugin $plugin, $asyncTask){
+		parent::__construct($plugin);
+		$this->asyncTask = $asyncTask;
+	}
+
+	/**
+	 * Actions to execute when run
+	 *
+	 * @param int $currentTick
+	 *
+	 * @return void
+	 */
+	public function onRun(int $currentTick){
+		Server::getInstance()->getScheduler()->scheduleAsyncTask($this->asyncTask);
+	}
+}
